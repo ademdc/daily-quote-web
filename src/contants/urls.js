08e75a6b67e83e1cc2501cfe48c139485ea7091e
Adem Dinarevic <ademdinarevic@gmail.com>
2020-03-24 13:33:32 +0100

    change base url

--- src/contants/urls.js
+++ src/contants/urls.js
@@ -1,5 +1,5 @@
 export default {
-    // base: 'https://afternoon-harbor-81797.herokuapp.com',
-    base: 'https://0964d976.ngrok.io',
+    base: 'https://afternoon-harbor-81797.herokuapp.com',
+    // base: 'https://0964d976.ngrok.io',
     customImage: 'https://wallpapercave.com/wp/wp2333214.jpg'
 };
